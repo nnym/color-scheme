c1af89d4c179aae5415fdaa3d3356cb088e894d8
Muhammad <tjmnkrajyej@gmail.com>
2023-12-27 16:43:24 +0000

    Add languages, fonts & editor colors; save edits.
    - Add `<title>`.
    - Add font family placeholder.
    - Fill in more colors.
    - Focus editor on load.
    - Focus hex inputs up or down by pressing arrow keys.
    - Clean up.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [Color Schemer](https://nnym.github.io/color-schemer) is a web application that facilitates the design of programming color schemes.
-It contains a CodeMirror editor prepopulated with sample code and a sidebar with responsive color parameters.
-The font can be configured. Google Fonts is searched if the specified font is not found.
+It contains a CodeMirror editor that supports many languages and is prepopulated with sample code (currently only C++)
+and a sidebar with responsive color parameters.
+The font can be changed and Google Fonts is searched if the specified font is not found.
--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-import "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.js";
 import * as cm from "codemirror";
 import * as cma from "@codemirror/autocomplete";
 import * as cmc from "@codemirror/commands";
-import {cpp} from "@codemirror/lang-cpp";
 import * as cml from "@codemirror/language";
+import {languages} from "@codemirror/language-data";
 import * as cmli from "@codemirror/lint";
 import * as cms from "@codemirror/state";
 import * as cmv from "@codemirror/view";
@@ -14,167 +13,175 @@ declare global {
 	interface Window {
 		setFontFamily: () => Promise<void>;
 		setFontSize: () => void;
+		setLanguage: () => void;
 	}
 }
+
+const FONT = "font";
+const FONT_SIZE = "fontSize";
+const LANGUAGE = "language";
 
 const {log} = console;
 // @ts-ignore
 const $ = (...args) => document.querySelector(...args) as HTMLElement;
 
-const gray = "808080";
-const gray05 = "d8d8d8";
-const white = "d0d0d0";
-const yellow = "eedd82";
-const green = "32cd32";
-const lightBlue = "6897bb";
-const darkGreen = "629755";
-const orange = "cc7832";
-const purple = "9876aa";
-const darkRed = "a34a27";
+const fontFamily = $("#font-family") as HTMLInputElement;
+const fontSize = $("#font-size") as HTMLInputElement;
+const languageSelect = $("#language") as HTMLSelectElement;
 
-const colors = {
-	comment: gray,
-	lineComment: gray,
-	blockComment: gray,
-	docComment: gray,
-	name: white,
-	variableName: white,
-	typeName: orange,
-	tagName: white,
-	propertyName: yellow,
-	attributeName: yellow,
-	className: white,
-	labelName: purple,
-	namespace: purple,
-	macroName: green,
-	literal: lightBlue,
-	string: darkGreen,
-	docString: darkGreen,
-	character: lightBlue,
-	attributeValue: yellow,
-	number: lightBlue,
-	integer: lightBlue,
-	float: lightBlue,
-	bool: lightBlue,
-	regexp: darkGreen,
-	escape: white,
-	color: darkGreen,
-	url: lightBlue,
-	keyword: orange,
-	self: orange,
-	null: lightBlue,
-	atom: null,
-	unit: null,
-	modifier: null,
-	operatorKeyword: orange,
-	controlKeyword: orange,
-	definitionKeyword: orange,
-	moduleKeyword: orange,
-	operator: gray05,
-	derefOperator: gray05,
-	arithmeticOperator: gray05,
-	logicOperator: gray05,
-	bitwiseOperator: gray05,
-	compareOperator: gray05,
-	updateOperator: gray05,
-	definitionOperator: gray05,
-	typeOperator: gray05,
-	controlOperator: gray05,
-	punctuation: white,
-	separator: white,
-	bracket: white,
-	angleBracket: white,
-	squareBracket: white,
-	paren: white,
-	brace: white,
-	content: white,
-	heading: white,
-	heading1: orange,
-	heading2: yellow,
-	heading3: darkRed,
-	heading4: gray,
-	heading5: purple,
-	heading6: darkGreen,
-	contentSeparator: null,
-	list: null,
-	quote: darkGreen,
-	emphasis: white,
-	strong: white,
-	link: null,
-	monospace: null,
-	strikethrough: null,
-	inserted: null,
-	deleted: null,
-	changed: null,
-	invalid: null,
-	meta: null,
-	documentMeta: null,
-	annotation: null,
-	processingInstruction: null,
-	definition: null,
-	constant: lightBlue,
-	function: yellow,
-	standard: null,
-	local: null,
-	special: green
+const previewKey = () => "preview/" + languageSelect.value;
+
+function map<V, T>(o: {[k: string]: V}, mapper: (key: string, value: V) => T): {[k: string]: T} {
+	for (let key in o) {
+		let value = mapper(key, o[key]);
+
+		if (value instanceof Array) {
+			delete o[key];
+			[key, value] = value;
+		}
+
+		(o as any)[key] = value;
+	}
+
+	return o as any as {string: T};
+}
+
+const gray = "#808080";
+const gray01 = "#282828";
+const gray02 = "#383838";
+const gray04 = "#b8b8b8";
+const gray05 = "#d8d8d8";
+const white = "#d0d0d0";
+const yellow = "#eedd82";
+const green = "#32cd32";
+const lightBlue = "#6897bb";
+const darkGreen = "#629755";
+const orange = "#cc7832";
+const purple = "#9876aa";
+const darkRed = "#a34a27";
+
+const scheme: {[k: string]: {[k: string]: string | null}} = {
+	editor: {
+		background: gray01,
+		selection: gray02,
+		caret: gray04
+	},
+	syntax: {
+		...map({...tags}, _ => null),
+		comment: gray,
+		lineComment: gray,
+		blockComment: gray,
+		docComment: gray,
+		name: white,
+		variableName: white,
+		typeName: orange,
+		tagName: white,
+		propertyName: yellow,
+		attributeName: yellow,
+		className: white,
+		labelName: purple,
+		namespace: purple,
+		macroName: green,
+		literal: lightBlue,
+		string: darkGreen,
+		docString: darkGreen,
+		character: lightBlue,
+		attributeValue: yellow,
+		number: lightBlue,
+		integer: lightBlue,
+		float: lightBlue,
+		bool: lightBlue,
+		regexp: darkGreen,
+		escape: white,
+		color: darkGreen,
+		url: lightBlue,
+		keyword: orange,
+		self: orange,
+		null: lightBlue,
+		atom: null,
+		unit: null,
+		modifier: gray05,
+		operatorKeyword: orange,
+		controlKeyword: orange,
+		definitionKeyword: orange,
+		moduleKeyword: orange,
+		operator: gray05,
+		derefOperator: gray05,
+		arithmeticOperator: gray05,
+		logicOperator: gray05,
+		bitwiseOperator: gray05,
+		compareOperator: gray05,
+		updateOperator: gray05,
+		definitionOperator: gray05,
+		typeOperator: gray05,
+		controlOperator: gray05,
+		punctuation: white,
+		separator: white,
+		bracket: white,
+		angleBracket: white,
+		squareBracket: white,
+		paren: white,
+		brace: white,
+		content: white,
+		heading: white,
+		heading1: orange,
+		heading2: yellow,
+		heading3: darkRed,
+		heading4: gray,
+		heading5: purple,
+		heading6: darkGreen,
+		contentSeparator: null,
+		list: null,
+		quote: darkGreen,
+		emphasis: white,
+		strong: white,
+		link: null,
+		monospace: null,
+		strikethrough: null,
+		inserted: null,
+		deleted: null,
+		changed: null,
+		invalid: null,
+		meta: null,
+		documentMeta: null,
+		annotation: null,
+		processingInstruction: orange,
+		definition: null,
+		constant: lightBlue,
+		function: yellow,
+		standard: null,
+		local: null,
+		special: green
+	}
 };
 
-const colorCompartment = new cms.Compartment;
+const compartment = new cms.Compartment;
 
 const view = new cmv.EditorView({
 	parent: document.body,
 	extensions: [
-		colorCompartment.of([]),
-		cpp(),
+		compartment.of([]),
 		cml.indentUnit.of("\t"),
 		cmv.keymap.of([cmc.indentWithTab]),
+		cmv.EditorView.updateListener.of(u => {
+			if (u.docChanged) localStorage.setItem(previewKey(), view.state.doc.toString());
+		}),
 		cm.basicSetup
-	],
-	doc: `
-		#include <cstdio>
-
-		template<typename T>
-		struct Node {
-			T value;
-			Node<T> *left, *right;
-
-			Node(T value = T(), Node<T> *left = nullptr, Node<T> *right = nullptr) : value(value), left(left), right(right) {}
-
-			void traverseInOrder(auto process) {
-				if (this->left) this->left->traverseInOrder(process);
-				process(*this);
-				if (this->right) this->right->traverseInOrder(process);
-			}
-
-			~Node() {
-				if (this->left) delete this->left;
-				if (this->right) delete this->right;
-			}
-		};
-
-		int main() {
-			auto l = new Node("foo");
-			auto rr = new Node("quux");
-			auto r = new Node<const char*>("baz", nullptr, rr);
-			Node root("bar", l, r);
-			root.traverseInOrder([](auto &node) {
-				puts(node.value);
-			});
-		}
-	`.replaceAll(/(?<=\n)\t{2}/g, "").trim()
+	]
 });
 
+view.focus();
+
 window.setFontFamily = async () => {
-	const input = $("#font-family") as HTMLInputElement;
-	input.value = input.value.trim();
+	fontFamily.value = fontFamily.value.trim();
+	localStorage.setItem(FONT, fontFamily.value);
 	const style = $(".cm-scroller")!.style;
 
-	if (!["", "serif", "sans-serif", "monospace"].includes(input.value.toLowerCase())) {
-		const fonts = await document.fonts.ready;
+	if (!["", "serif", "sans-serif", "monospace"].includes(fontFamily.value.toLowerCase())) {
 		const urlPrefix = "https://fonts.googleapis.com/css?family=";
-		const correctedURL = urlPrefix + input.value.replaceAll(/(?<=\b)\p{L}/gu, m => m[0].toUpperCase());
+		const correctedURL = urlPrefix + fontFamily.value.replaceAll(/(?<=\b)\p{L}/gu, m => m[0].toUpperCase());
 
-		for (const url of new Set([correctedURL, urlPrefix + input.value])) {
+		for (const url of new Set([correctedURL, urlPrefix + fontFamily.value])) {
 			const link = document.createElement("link");
 			link.rel = "stylesheet";
 			link.href = url;
@@ -182,52 +189,108 @@ window.setFontFamily = async () => {
 		}
 	}
 
-	style.fontFamily = input.value + (input.value && ", ") + "monospace";
+	style.fontFamily = fontFamily.value + (fontFamily.value && ", ") + "monospace";
 };
 
 window.setFontSize = () => {
-	const input = $("#font-size") as HTMLInputElement;
-	input.value = input.value.replaceAll(/[^\d]/g, "");
-	if (input.value.length) $(".cm-content")!.style.fontSize = input.value + "px";
+	fontSize.value = fontSize.value.replaceAll(/[^\d]/g, "");
+	localStorage.setItem(FONT_SIZE, fontSize.value);
+	if (fontSize.value.length) $(".cm-content")!.style.fontSize = fontSize.value + "px";
 };
 
-function applyHighlighting() {
-	view.dispatch({effects: colorCompartment.reconfigure(cml.syntaxHighlighting(cml.HighlightStyle.define(
-		Object.entries(colors).map(([name, color]) => ({tag: tags[name], color: "#" + color}))
-	)))});
+window.setLanguage = async () => {
+	const {value} = languageSelect;
+	localStorage.setItem(LANGUAGE, value);
+	const doc = localStorage.getItem(previewKey()) || await fetch(previewKey()).then(r => r.text()).catch(_ => "");
+	// @ts-ignore
+	view.dispatch({changes: [{from: 0, to: view.state.doc.length, insert: doc}]})
+	applyHighlighting();
 }
 
+async function applyHighlighting() {
+	view.dispatch({effects: compartment.reconfigure([
+		await languages.find(l => l.alias.includes(languageSelect.value))!.load(),
+		cmv.EditorView.theme({
+			"&, .cm-gutters": {
+				background: scheme.editor.background
+			},
+			".cm-cursor": {
+				borderColor: scheme.editor.caret
+			},
+			"&.cm-focused:not(#_) .cm-selectionBackground": {
+				background: scheme.editor.selection
+			}
+		}, {dark: true}),
+		// @ts-ignore
+		cml.syntaxHighlighting(cml.HighlightStyle.define(Object.entries(scheme.syntax).map(([name, color]) => ({tag: tags[name], color}))))
+	])});
+}
+
+const brain = languages.find(l => l.extensions.includes("bf"));
+// @ts-ignore
+[brain.name, brain.alias] = ["Brain****", ["brain****"]];
+
+for (const language of languages.sort((a, b) => a.name.localeCompare(b.name))) {
+	const option = document.createElement("option");
+	option.value = language.alias[0];
+	option.textContent = language.name;
+	languageSelect.appendChild(option);
+}
+
+languageSelect.value = localStorage.getItem(LANGUAGE) ?? "cpp";
+languageSelect.oninput = window.setLanguage;
+
+fontFamily.value = localStorage.getItem(FONT) ?? "";
+fontSize.value = localStorage.getItem(FONT_SIZE) ?? fontSize.value;
 window.setFontFamily();
 window.setFontSize();
-applyHighlighting();
+window.setLanguage();
 
 const colorList = $("#colors")!;
+const hexInputs: HTMLInputElement[] = [];
 
-for (const name in tags) {
-	const hex = document.createElement("input");
-	hex.type = "text";
-	hex.value = "#" + (colors[name] ?? "");
-	hex.maxLength = 7;
+for (const set of Object.values(scheme)) {
+	for (const name in set) {
+		const hex = document.createElement("input");
+		hex.type = "text";
+		hex.value = set[name] ?? "";
 
-	const color = document.createElement("input");
-	color.type = "color";
-	color.value = hex.value
+		const index = hexInputs.length;
+		hexInputs.push(hex);
 
-	hex.oninput = e => {
-		const value = hex.value.trim();
-		hex.value = value.startsWith("#") ? value : ("#" + value).substring(0, hex.maxLength);
-		color.value = hex.value;
-		colors[name] = hex.value.substring(1);
-		applyHighlighting();
-	};
+		const color = document.createElement("input");
+		const setColor = () => color.value = hex.value.length == 4 ? "#" + [...hex.value.substring(1)].map(c => c + c).join("") : hex.value;
+		color.type = "color";
+		setColor();
 
-	color.oninput = e => {
-		hex.value = color.value;
-		colors[name] = hex.value.substring(1);
-		applyHighlighting();
-	};
+		hex.oninput = _ => {
+			const value = hex.value.trim();
+			hex.value = value.replace(/^#*/, "#").substring(0, 7);
+			setColor();
+			set[name] = hex.value;
+			applyHighlighting();
+		};
 
-	colorList.appendChild(document.createTextNode(name.split(/(?<=[a-z])(?=[A-Z\d])/).map(s => s.toLowerCase()).join(" ")));
-	colorList.appendChild(hex);
-	colorList.appendChild(color);
+		hex.onkeydown = e => {
+			const target =
+				e.key == "ArrowDown" ? hexInputs[index + 1]
+				: e.key == "ArrowUp" ? hexInputs[index - 1]
+				: null;
+
+			if (target) {
+				target.focus();
+				target.selectionStart = target.selectionEnd = target.value.length;
+				return false;
+			}
+		};
+
+		color.oninput = _ => {
+			hex.value = color.value;
+			set[name] = hex.value;
+			applyHighlighting();
+		};
+
+		colorList.append(document.createTextNode(name.replaceAll(/(?<=[a-z])[A-Z\d]/g, m => " " + m[0].toLowerCase())));
+		colorList.append(hex, color);
+	}
 }
